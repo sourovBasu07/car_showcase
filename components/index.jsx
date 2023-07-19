498f9540b4b338e8e93d124dfb17eb742d19ee21
sourovBasu07 <souravbasu123456@gmail.com>
2023-07-19 21:59:30 +0600

    initial commit

---
+++ components/index.jsx
@@ -0,0 +1,22 @@
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
+CarDetails;
+import Showmore from "./Showmore";
+
+export {
+  Hero,
+  Navbar,
+  Footer,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+  CarCard,
+  CarDetails,
+  Showmore,
+};
---
+++ utils/index.js
@@ -0,0 +1,60 @@
+export async function fetchCars(filters) {
+  const { manufacturer, year, limit, fuel, model } = filters;
+
+  const headers = {
+    "X-RapidAPI-Key": "c313cdfd9dmsh5a513cd86bc8f1ap1db8b4jsn15af85be3afb",
+    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+  };
+
+  const response = await fetch(
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&model=${model}&fuel_type=${fuel}&limit=${limit}&year=${year}`,
+    {
+      headers: headers,
+    }
+  );
+
+  const result = await response.json();
+
+  return result;
+}
+
+export const updateSearchParams = (type, value) => {
+  const searchParams = new URLSearchParams(window.location.search);
+
+  searchParams.set(type, value);
+
+  const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
+
+  return newPathname;
+};
+
+export const calculateCarRent = (city_mpg, year) => {
+  const basePricePerDay = 50;
+  const mileageFactor = 0.1;
+  const ageFactor = 0.05;
+
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+  return rentalRatePerDay.toFixed(0);
+};
+
+export const generateCarImage = (car) => {
+  const url = new URL("https://cdn.imagin.studio/getimage");
+  const { make, model, year } = car;
+
+  url.searchParams.append(
+    "customer",
+    process.env.NEXT_PUBLIC_IMAGIN_API_KEY || ""
+  );
+  url.searchParams.append("make", make);
+  url.searchParams.append("modelFamily", model.split(" ")[0]);
+  url.searchParams.append("zoomType", "fullscreen");
+  url.searchParams.append("modelYear", `${year}`);
+  // url.searchParams.append('zoomLevel', zoomLevel);
+  // url.searchParams.append("angle", `${angle}`);
+
+  return `${url}`;
+};
